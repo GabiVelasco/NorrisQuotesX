fbc51185b89f056aaf48d36d5e230dfa0e3ba5d7
Gabriela Velasco <gabriela.luppe@gmail.com>
2024-02-11 22:46:26 +0100

    update counter added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,32 @@
 import Image from "next/image";
 import prisma from "./utils/prisma";
 
-
 export default async function Home() {
+  // Increment visitor number in the database
+  await prisma.visitorCounter.update({
+    where: { id: 1 }, // Assuming id 1 is the specific visitor you want to update
+    data: {
+      visitorNumber: {
+        increment: 1
+      }
+    }
+  });
+
+  // Fetch all visitor numbers
   const visitors = await prisma.visitorCounter.findMany();
+  
+  // Calculate the sum of visitor numbers
+  const sumVisitorNumbers = visitors.reduce((acc, visitor) => acc + visitor.visitorNumber, 0);
+
   return (
     <main>
-    <h1 className="font-bold">Visitors</h1>
-    <ul>
-      {visitors.map((visitorCounter) => (
-        <li key={visitorCounter.id}>{visitorCounter.visitorNumber}</li>
-      ))}
-    </ul>
-  </main>
+      <h1 className="font-bold">Visitors</h1>
+      <ul>
+        {visitors.map((visitorCounter) => (
+          <li key={visitorCounter.id}>{visitorCounter.visitorNumber}</li>
+        ))}
+        Total: {sumVisitorNumbers}
+      </ul>
+    </main>
   );
 }
