05ec0b5fe1cf06003fc4afba9c1e2997bc746618
Gabriela Velasco <gabriela.luppe@gmail.com>
2024-02-12 16:16:43 +0100

    refactoring and docstring in home page added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,41 @@
-import prisma from "./utils/prisma";
-import "./quotes/page"
-import { Zitat, Button } from "./quotes/page";
-import "./header/page"
+/**
+ * This page component represents the home page of the application.
+ * It includes a layout with a header, footer, and a main section.
+ * Within the main section, it displays an image and a Chuck Norris quote component.
+ * The layout is structured using a Layout component.
+ */
+
+import Image from "next/image";
+import bild4 from '../app/images/bild4.jpg';
 import Header from "./header/page";
 import Footer from "./footer/page";
+import { Zitat } from "./quotes/page";
+import "./quotes/page";
+import "./header/page";
+import "./footer/page";
+import Layout from "./layout";
 
+/**
+ * Home page component.
+ * @returns {ReactElement} The home page layout.
+ */
 export default async function Home() {
-  // Increment visitor number in the database
-  await prisma.visitorCounter.update({
-    where: { id: 1 }, // Assuming id 1 is the specific visitor you want to update
-    data: {
-      visitorNumber: {
-        increment: 1
-      }
-    }
-  });
-
-  // Fetch all visitor numbers
-  const visitors = await prisma.visitorCounter.findMany();
-  
-  // Calculate the sum of visitor numbers
-  const sumVisitorNumbers = visitors.reduce((acc, visitor) => acc + visitor.visitorNumber, 0);
-
   return (
-  
-
-    <main>
-       <Header />
-       
-      <h1 className="font-bold">Visitors</h1>
-      <Zitat />
-      <ul>
-        {visitors.map((visitorCounter) => (
-          <li key={visitorCounter.id}>{visitorCounter.visitorNumber}</li>
-        ))}
-        Total: {sumVisitorNumbers}
-      </ul>
-
-      <Footer />
-    </main>
+    <Layout>
+      <Header/>
+      <main className="justify-center place-content-center items-center h-screen items-stretch">
+        <div className="max-w-[1000px] mx-auto">
+          <div className="container relative">
+            <div className="absolute justify-start items-center gap-80px inline-flex">
+              <Image className="image" src={bild4} alt="Bild 4" width={350} height={350} />
+              <div className="text-center">
+                <Zitat/><br />
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+      <Footer/>
+    </Layout>
   );
 }
