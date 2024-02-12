dbc9ab4b3f138d92bae5e73a018d3ef01080ce75
Gabriela Velasco <gabriela.luppe@gmail.com>
2024-02-12 11:14:12 +0100

    footer created

---
+++ src/app/footer.tsx
@@ -0,0 +1,13 @@
+// Footer.tsx
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <footer>
+      {/* Your footer content goes here */}
+      <p>This is the footer</p>
+    </footer>
+  );
+};
+
+export default Footer;
