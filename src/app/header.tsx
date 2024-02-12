67dfd350b560d510da6986a2416be1c599773b22
Gabriela Velasco <gabriela.luppe@gmail.com>
2024-02-12 11:14:31 +0100

    header created

---
+++ src/app/header.tsx
@@ -0,0 +1,13 @@
+// Header.tsx
+import React from "react";
+
+const Header: React.FC = () => {
+  return (
+    <header>
+      {/* Your header content goes here */}
+      <h1>This is the header</h1>
+    </header>
+  );
+};
+
+export default Header;
